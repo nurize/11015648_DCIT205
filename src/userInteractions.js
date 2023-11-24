9371e445ca98a3c674a28f6a7d52681bb26ccca3
Eunice Dickson <sunshinebae439@gmail.com>
2023-11-24 18:39:33 +0000

    Updated some pages and added a few new pages

---
+++ src/userInteractions.js
@@ -0,0 +1,11 @@
+const dropdowns =  document.querySelectorAll('.options');
+
+//Loop through all dropdown elements
+dropdowns.forEach(dropdown => {
+    //Get inner elements for each dropdown
+    const dropdownbutton = dropdown.querySelector('.dropdownbutton');
+    const caret = dropdown.querySelector('.caret');
+    const select = dropdown.querySelector('.select');
+    const menu = dropdown.querySelector('.menu');
+}
+    )
